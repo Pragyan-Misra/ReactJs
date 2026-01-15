85a3e65d36ee1c27b30165e077806c7116103543
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-15 17:07:07 +0530

    update

--- 06currenyconverter/src/components/InputBox.jsx
+++ 06currenyconverter/src/components/InputBox.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-
+import React, {useId} from 'react'
+// useId hook, React hook for generating unique IDs that can be passed accessibility attribute. Do not use useId to generate Keys in list.
 
 function InputBox({
     label,
     amount,
     onAmountChange,
     onCurrencyChange,
-    currencyOption = [],
+    currencyOptions = [],
     selectCurrency = "usd",
     amountDisable = false,
     currencyDisable = false,
     className = "",
 }) {
-   
+   const amountInputId = useId();
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -34,13 +34,17 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled = {currencyDisable}
                     
                 >
                     
-                        <option value="usd">
-                            usd
-                        </option>
-                
+                        {currencyOptions.map((currency) => { 
+                            return <option key={currency} value={currency}>
+                            {currency}
+                        </option>})}
+                {/* to use loops in jsx always provide key. */}
                 </select>
             </div>
         </div>
---
+++ 06currenyconverter/src/components/index.js
@@ -0,0 +1,5 @@
+import InputBox from "./InputBox";
+
+export {InputBox};
+
+// better practise to make a index file in component and import all the components and export all, so that when we need component we only need to call one file for all components not all files seperately.
