65a8d20e36ff74cbdbcf686251c643603db0af6a
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-18 22:55:31 +0530

    update

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   
   return (
     <>
-      
+      <h1>React Router</h1>
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem omnis at tempora id esse laudantium autem laboriosam, obcaecati placeat corrupti harum debitis perspiciatis, neque consequuntur iste non nobis laborum quaerat voluptates aperiam veritatis deleniti voluptatem optio officiis? A voluptatibus, deserunt expedita tempora ratione harum recusandae. Consequuntur tempore dolorum corrupti, labore similique neque aut animi atque nemo nisi in expedita mollitia earum aliquam minima laudantium nesciunt sunt ut libero est dolore? Laboriosam, harum ipsam saepe eius sunt ipsum sed, sapiente quis consequatur, maiores culpa incidunt? Dignissimos sapiente a voluptate inventore praesentium, est ut quaerat quasi ex explicabo dolorem corrupti commodi magnam!</p>
+      </div>
     </>
   )
 }
