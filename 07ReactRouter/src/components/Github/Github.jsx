b7987908d4cda4ebd0c7846a43dc33369b643b71
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-21 15:42:48 +0530

    update

--- 07ReactRouter/src/components/Github/Github.jsx
+++ 07ReactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+  const data = useLoaderData()
+  // const [data, setdata] =useState([])
+  // useEffect(() => {
+  //   fetch(`https://api.github.com/users/${}`)
+  //   .then( res => res.json())
+  //   .then( data => {console.log(data);
+  //     setdata(data)
+  //   })
+  // }, [])
   return (
-    <div className='text-center bg-gray-600 text-white p-4 text-3xl'>Github Followers</div>
+    <div className='text-center bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data?.followers}
+    <img src={data?.avatar_url} alt="Git Picture"  width={300}/>
+    </div>
   )
 }
 
 export default Github
+
+export const githubLoader = async () => {
+   const response = await fetch(`https://api.github.com/users/Pragyan-Misra`);
+
+   return response.json();
+}
