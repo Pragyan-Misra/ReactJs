f8c499d19fa2c08c1434edf095c1b5d06ca12152
Pragyan Misra <pragyanmisra080625@gmail.com>
2026-01-25 22:29:26 +0530

    update

---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function UserContextProvide() {
+  const [cont, countstate ] = useState(0);
+  return(
+    <h1>HELLO</h1>
+    
+  )
+}
+
+export default UserContextProvide;
